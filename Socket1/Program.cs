33a3b12a88db34efa4744685c5d7d141191d91fa
LizethMendieta <lizlumendieta@gmail.com>
2020-10-29 14:22:50 -0600

    Agregar archivos de proyecto.

---
+++ Socket1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket;
+
+namespace Socket1
+{
+    class Program
+    {
+        private static WebSocketServer servidorSocket;
+        static void Main(string[] args)
+        {
+            servidorSocket = new WebSocketServer();
+            int puerto = 8088;
+            servidorSocket.Setup(puerto);
+            servidorSocket.NewSessionConnected += servidorSocket.NewSessionConnected
+                
+        }
+    }
+}
